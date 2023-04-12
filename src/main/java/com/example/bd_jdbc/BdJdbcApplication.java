93226040b992336d767a8e32bce691c8da00a3ab
sergeybryan <regkmastis@yandex.ru>
2023-04-12 15:17:38 +0300

    Initial commit

---
+++ src/main/java/com/example/bd_jdbc/BdJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.bd_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bd_jdbc/BdJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bd_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
